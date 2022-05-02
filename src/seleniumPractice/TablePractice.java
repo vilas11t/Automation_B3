340787d87ffde3f4893f0ebf9607ea936103001e
aartidalvi14 <dalviaarti81@gmail.com>
2022-05-02 21:33:27 +0530

    Right Table

--- src/seleniumPractice/TablePractice.java
+++ src/seleniumPractice/TablePractice.java
@@ -13,6 +13,7 @@ public static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		driver=driverinilialize();
 		tableAddColumnValues();
+		tableAddRightColumnValues();
 	}
 	
 	static void tableAddColumnValues() {
@@ -28,7 +29,23 @@ public static WebDriver driver;
 		
 		System.out.println("Total price:="+sum);
 		
-		driver.close();
+		//driver.close();
+	}
+		
+		static void tableAddRightColumnValues() {
+			driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+			List<WebElement> amount=driver.findElements(By.xpath("//div[@class=\"tableFixHead\"]//tbody//tr//td[4]"));
+			
+			int sum=0;
+			
+			for(WebElement w:amount) {
+				int a=Integer.parseInt(w.getText());
+				sum=sum+a;
+			}
+			
+			System.out.println("Total Right Table Amount:="+sum);
+			
+			driver.close();
 		
 	}
 	
